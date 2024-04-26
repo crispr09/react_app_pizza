dc9bcb5c09c83c883fb4520d112da24bf303f8a0
sachin <contact2sachin11@gmail.com>
2024-04-26 12:39:53 +0530

    full project completed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,128 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client"
+import './index.css'
+const pizzaData = [
+    {
+      name: "Focaccia",
+      ingredients: "Bread with italian olive oil and rosemary",
+      price: 6,
+      photoName: "pizzas/focaccia.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Margherita",
+      ingredients: "Tomato and mozarella",
+      price: 10,
+      photoName: "pizzas/margherita.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Spinaci",
+      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+      price: 12,
+      photoName: "pizzas/spinaci.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Funghi",
+      ingredients: "Tomato, mozarella, mushrooms, and onion",
+      price: 12,
+      photoName: "pizzas/funghi.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Salamino",
+      ingredients: "Tomato, mozarella, and pepperoni",
+      price: 15,
+      photoName: "pizzas/salamino.jpg",
+      soldOut: true,
+    },
+    {
+      name: "Pizza Prosciutto",
+      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+      price: 18,
+      photoName: "pizzas/prosciutto.jpg",
+      soldOut: false,
+    },
+  ];
+  
+function Awpp() {
+    return (
+    <div className="container">
+        <h1>Hello from my project react!!</h1>
+        <Header />
+        <Menu />
+        <Footer />
+    </div>
+    );
+}
+function Header() {
+    return ( 
+        <header className="header">
+        <h1 style={{color:"red", fontSize:"48px", textTransform: "uppercase"}}>Fast Pizza Devivery co.</h1>
+        </header>
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    );
+}
+function Menu() {
+    const pizzas = pizzaData;
+    // const pizzas = [];
+    const numPizza = pizzas.length;
+    return (
+        <main className="menu">
+            <h2>Our Pizzas</h2>
+            {numPizza >0 ? (
+            <ul className="pizzas">
+                {pizzas.map(pizza => (
+                    <Pizza pizzaObj={pizza} key={pizza.name} />
+
+                ))}
+            </ul>
+            ) : <p>We'are still working on our today's menu.Please visit in sometime</p>}
+            
+      </main>
+    );
+}
+
+function Pizza(a) {
+    // if(a.pizzaObj.soldOut) return null
+    return (
+    <li className={`pizza ${a.pizzaObj.soldOut ? "sold-out" : ""}`}>
+        
+        <img src={a.pizzaObj.photoName}></img>
+        <div>
+            <h3>{a.pizzaObj.name}</h3>
+            <p>{a.pizzaObj.ingredients}</p>
+
+            <span>{a.pizzaObj.soldOut ? "SOLD OUT" : a.pizzaObj.price}</span>
+        </div>
+    </li>
 );
+}
+function Footer() {
+    const hour = new Date().getHours();
+    const openHour = 21;
+    const closeHour = 22;
+    const isOpen = hour>= openHour && hour <=closeHour;
+    // if(!isOpen) 
+    // return (
+    // <p>We're are happy to welcome you between {openHour} and {closeHour} </p>
+    // );
+    return (
+    <footer className="footer">
+        <div className="order">
+            {
+                <p>We're currently open. Come visit us or order online </p>
+            }
+             <button className="btn"> Order</button>
+        </div>
+    </footer>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <Awpp />
+    </React.StrictMode>
+);
